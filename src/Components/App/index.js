3273f8265de24e46db961cde53e7e98144a7092c
Elvis <elvisgiltorres@gmail.com>
2022-04-03 15:15:51 -0400

    Cambiando la ruta de la API

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -21,7 +21,7 @@ const App = () => {
 
                 
                 {initialState.length !== 0 &&
-                    initialState.myList.length > 0  &&
+                    initialState.myList.length !== 0  &&
                         <Categorias tittle="Mi lista">
                             <Carousel>
                                 {initialState.myList.map(item => (
